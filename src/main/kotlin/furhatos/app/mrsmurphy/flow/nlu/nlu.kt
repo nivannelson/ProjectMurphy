98a4b7f3efce8c51a27b2ca434fe723e134e4de5
Albatross-hack <nivroll60@gmaill.com>
2023-03-04 12:18:53 +0000

    initial commit

--- src/main/kotlin/furhatos/app/mrsmurphy/flow/nlu/nlu.kt
+++ src/main/kotlin/furhatos/app/mrsmurphy/flow/nlu/nlu.kt
@@ -1,2 +1,12 @@
 package furhatos.app.mrsmurphy.flow.nlu
 
+import furhatos.nlu.Intent
+import furhatos.util.Language
+
+class NationalRobotarium : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf(
+            "National robotatium",
+            "this building")
+    }
+}
