25ff18e4a4a0e78ae89c0e2bf5eacce304cb8865
Albatross-hack <nivroll60@gmaill.com>
2023-03-04 12:14:26 +0000

    initial commit

---
+++ src/main/kotlin/furhatos/app/mrsmurphy/flow/init.kt
@@ -0,0 +1,29 @@
+package furhatos.app.mrsmurphy.flow
+
+import furhatos.app.mrsmurphy.flow.main.Idle
+import furhatos.app.mrsmurphy.flow.main.Greeting
+import furhatos.app.mrsmurphy.setting.DISTANCE_TO_ENGAGE
+import furhatos.app.mrsmurphy.setting.MAX_NUMBER_OF_USERS
+import furhatos.flow.kotlin.State
+import furhatos.flow.kotlin.furhat
+import furhatos.flow.kotlin.state
+import furhatos.flow.kotlin.users
+
+val Init: State = state {
+    init {
+        /** Set our default interaction parameters */
+        users.setSimpleEngagementPolicy(DISTANCE_TO_ENGAGE, MAX_NUMBER_OF_USERS)
+    }
+    onEntry {
+        /** start interaction */
+        when {
+            furhat.isVirtual() -> goto(Greeting) // Convenient to bypass the need for user when running Virtual Furhat
+            users.hasAny() -> {
+                furhat.attend(users.random)
+                goto(Greeting)
+            }
+            else -> goto(Idle)
+        }
+    }
+
+}
---
+++ src/main/kotlin/furhatos/app/mrsmurphy/main.kt
@@ -0,0 +1,15 @@
+package furhatos.app.mrsmurphy
+
+import furhatos.app.mrsmurphy.flow.Init
+import furhatos.flow.kotlin.Flow
+import furhatos.skills.Skill
+
+class MrsmurphySkill : Skill() {
+    override fun start() {
+        Flow().run(Init)
+    }
+}
+
+fun main(args: Array<String>) {
+    Skill.main(args)
+}
