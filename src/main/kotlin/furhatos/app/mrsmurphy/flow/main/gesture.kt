c2215e7bcbf45a6ca10beee9330a173fcf882cad
Rohith <rohithkannan007@gmail.com>
2023-03-12 18:14:24 +0530

    response fix

--- src/main/kotlin/furhatos/app/mrsmurphy/flow/main/gesture.kt
+++ src/main/kotlin/furhatos/app/mrsmurphy/flow/main/gesture.kt
@@ -3,22 +3,30 @@ import furhatos.gestures.Gestures
 
 fun gesture(code: String): State = state {
     onEntry {
-        when (code) {
-            "[B@6996db8" -> furhat.gesture(Gestures.BigSmile)
-            "[B@1963006a" -> furhat.gesture(Gestures.Blink)
-            "[B@7fbe847c" -> furhat.gesture(Gestures.BrowFrown)
-            "[B@41975e01" -> furhat.gesture(Gestures.BrowRaise)
-            "[B@c2e1f26" -> furhat.gesture(Gestures.CloseEyes) //remove it later or open the eyes
-            "[B@dcf3e99" -> furhat.gesture(Gestures.ExpressAnger)
-            "[B@7dc5e7b4" -> furhat.gesture(Gestures.ExpressDisgust)
-            "[B@1ee0005" -> furhat.gesture(Gestures.ExpressFear)
-            "[B@75a1cd57" -> furhat.gesture(Gestures.ExpressSad)
-            "[B@6f2b958e" -> furhat.gesture(Gestures.OpenEyes)
-            "[B@626b2d4a" -> furhat.gesture(Gestures.Thoughtful)
-            "[B@1c4af82c" -> furhat.gesture(Gestures.Wink)
-            else -> { // Note the block
-                return
-            }
+        if(Assert.assertTrue(code == "[B@6996db8")
+            furhat.gesture(Gestures.BigSmile)
+        else if(Assert.assertTrue(code == "[B@1963006a")
+            furhat.gesture(Gestures.Blink)
+        else if(Assert.assertTrue(code == "[B@7fbe847c")
+            furhat.gesture(Gestures.BrowFrown)
+        else if(Assert.assertTrue(code == "[B@41975e01")
+            furhat.gesture(Gestures.BrowRaise)
+        else if(Assert.assertTrue(code == "[B@c2e1f26")
+            furhat.gesture(Gestures.CloseEyes) //remove it later or open the eyes
+        else if(Assert.assertTrue(code == "[B@dcf3e99")
+            furhat.gesture(Gestures.ExpressAnger)
+        else if(Assert.assertTrue(code == "[B@7dc5e7b4")
+            furhat.gesture(Gestures.ExpressDisgust)
+        else if(Assert.assertTrue(code == "[B@1ee0005")
+            furhat.gesture(Gestures.ExpressFear)
+        else if(Assert.assertTrue(code == "[B@75a1cd57")
+            furhat.gesture(Gestures.ExpressSad)
+        else if(Assert.assertTrue(code == "[B@6f2b958e")
+            furhat.gesture(Gestures.OpenEyes)
+        else if(Assert.assertTrue(code == "[B@626b2d4a")
+            furhat.gesture(Gestures.Thoughtful)
+        else if(Assert.assertTrue(code == "[B@1c4af82c")
+            furhat.gesture(Gestures.Wink)
         }
     }
 }
