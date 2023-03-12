92df39c14296ab8629f89728aa1509c2bc3c843e
Rohith <rohithkannan007@gmail.com>
2023-03-12 17:44:44 +0530

    adding gesture

--- src/main/kotlin/furhatos/app/mrsmurphy/flow/main/gesture.kt
+++ src/main/kotlin/furhatos/app/mrsmurphy/flow/main/gesture.kt
@@ -1,7 +1,27 @@
 import furhatos.flow.kotlin.furhat
+import furhatos.gestures.Gestures
+
+fun gesture(code: String){
+    when (code) {
+        "[B@6996db8" -> furhat.gesture(Gestures.BigSmile)
+        "[B@1963006a" -> furhat.gesture(Gestures.Blink)
+        "[B@7fbe847c" -> furhat.gesture(Gestures.BrowFrown)
+        "[B@41975e01" -> furhat.gesture(Gestures.BrowRaise)
+        "[B@c2e1f26" -> furhat.gesture(Gestures.CloseEyes) //remove it later or open the eyes
+        "[B@dcf3e99" -> furhat.gesture(Gestures.ExpressAnger)
+        "[B@7dc5e7b4" -> furhat.gesture(Gestures.ExpressDisgust)
+        "[B@1ee0005" -> furhat.gesture(Gestures.ExpressFear)
+        "[B@75a1cd57" -> furhat.gesture(Gestures.ExpressSad)
+        "[B@6f2b958e" -> furhat.gesture(Gestures.OpenEyes)
+        "[B@626b2d4a" -> furhat.gesture(Gestures.Thoughtful)
+        "[B@1c4af82c" -> furhat.gesture(Gestures.Wink)
+        else -> { // Note the block
+            return
+        }
+    }
+}
 
 fun parseResponse(response: String){
-    val response = "I love pizza 🍕 and I wanna have pizza now 🤤 checking now"
     val regex = Regex("((?<=;)|(?=;))")
     val emojiRegex = Regex("[\\p{So}]")
     val unicode = StringBuilder()
