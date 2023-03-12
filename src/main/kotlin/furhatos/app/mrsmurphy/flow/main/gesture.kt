d47c64e3cbdcefc233396349b0674572d44a8007
Rohith <rohithkannan007@gmail.com>
2023-03-12 18:23:12 +0530

    colmn fix

--- src/main/kotlin/furhatos/app/mrsmurphy/flow/main/gesture.kt
+++ src/main/kotlin/furhatos/app/mrsmurphy/flow/main/gesture.kt
@@ -1,37 +1,46 @@
 import furhatos.flow.kotlin.furhat
 import furhatos.gestures.Gestures
-import furhatos.flow.kotlin.State
-import furhatos.flow.kotlin.state
-import org.junit.Assert
 
 fun gesture(code: String): State = state {
     onEntry {
-        if(Assert.assertTrue(code == "[B@6996db8")
-            furhat.gesture(Gestures.BigSmile)
-        else if(Assert.assertTrue(code == "[B@1963006a")
-            furhat.gesture(Gestures.Blink)
-        else if(Assert.assertTrue(code == "[B@7fbe847c")
-            furhat.gesture(Gestures.BrowFrown)
-        else if(Assert.assertTrue(code == "[B@41975e01")
-            furhat.gesture(Gestures.BrowRaise)
-        else if(Assert.assertTrue(code == "[B@c2e1f26")
-            furhat.gesture(Gestures.CloseEyes) //remove it later or open the eyes
-        else if(Assert.assertTrue(code == "[B@dcf3e99")
-            furhat.gesture(Gestures.ExpressAnger)
-        else if(Assert.assertTrue(code == "[B@7dc5e7b4")
-            furhat.gesture(Gestures.ExpressDisgust)
-        else if(Assert.assertTrue(code == "[B@1ee0005")
-            furhat.gesture(Gestures.ExpressFear)
-        else if(Assert.assertTrue(code == "[B@75a1cd57")
-            furhat.gesture(Gestures.ExpressSad)
-        else if(Assert.assertTrue(code == "[B@6f2b958e")
-            furhat.gesture(Gestures.OpenEyes)
-        else if(Assert.assertTrue(code == "[B@626b2d4a")
-            furhat.gesture(Gestures.Thoughtful)
-        else if(Assert.assertTrue(code == "[B@1c4af82c")
-            furhat.gesture(Gestures.Wink)
-        }
+        if(Assert.assertTrue(code == "[B@6996db8"){
+                furhat.gesture(Gestures.BigSmile)
+            }
+        else if(Assert.assertTrue(code == "[B@1963006a"){
+                furhat.gesture(Gestures.Blink)
+            }
+        else if(Assert.assertTrue(code == "[B@7fbe847c"){
+                furhat.gesture(Gestures.BrowFrown)
+            }
+        else if(Assert.assertTrue(code == "[B@41975e01"){
+                furhat.gesture(Gestures.BrowRaise)
+            }
+        else if(Assert.assertTrue(code == "[B@c2e1f26"){
+                furhat.gesture(Gestures.CloseEyes) //remove it later or open the eyes
+            }
+        else if(Assert.assertTrue(code == "[B@dcf3e99"){
+                furhat.gesture(Gestures.ExpressAnger)
+            }
+        else if(Assert.assertTrue(code == "[B@7dc5e7b4"){
+                furhat.gesture(Gestures.ExpressDisgust)
+            }
+        else if(Assert.assertTrue(code == "[B@1ee0005"){
+                furhat.gesture(Gestures.ExpressFear)
+            }
+        else if(Assert.assertTrue(code == "[B@75a1cd57"){
+                furhat.gesture(Gestures.ExpressSad)
+            }
+        else if(Assert.assertTrue(code == "[B@6f2b958e"){
+                furhat.gesture(Gestures.OpenEyes)
+            }
+        else if(Assert.assertTrue(code == "[B@626b2d4a"){
+                furhat.gesture(Gestures.Thoughtful)
+            }
+        else if(Assert.assertTrue(code == "[B@1c4af82c"){
+                furhat.gesture(Gestures.Wink)
+            }
     }
+    terminate()
 }
 
 fun parseResponse(response: String): State = state {
@@ -56,4 +65,5 @@ fun parseResponse(response: String): State = state {
             }
         }
     }
+    terminate()
 }
