74942825d8d63cf858f6257ce5d9d4af12b9c066
Rohith <rohithkannan007@gmail.com>
2023-03-13 16:22:43 +0530

    Removing unwanted code

--- src/main/kotlin/furhatos/app/mrsmurphy/flow/main/chatgpt.kt
+++ src/main/kotlin/furhatos/app/mrsmurphy/flow/main/chatgpt.kt
@@ -10,48 +10,6 @@ import java.io.File
 
 val serviceKey = "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"
 
-
-/*fun getOpenDomainResponse(): String {
-    var response = ""
-    val service = OpenAiService(serviceKey)
-    val history = Furhat.dialogHistory.all.takeLast(10).mapNotNull {
-        when (it) {
-            is DialogHistory.ResponseItem -> {
-                "Human: ${it.response.text}"
-            }
-            is DialogHistory.UtteranceItem -> {
-                "Murphy: ${it.toText()}"
-            }
-            else -> null
-        }
-    }.joinToString(separator = "\n")
-    val prompt=history + "\n Use the above conversation to create an informal response that can be used by a social receptionist to say it as a response to a user without greeting them or wishing within 50 words."
-    var lengthofprompt=prompt.length
-    println(prompt)
-// Read more about these settings: https://beta.openai.com/docs/introduction
-    var temperature = 0.9 // Higher values means the model will take more risks. Try 0.9 for more creative applications, and 0 (argmax sampling) for ones with a well-defined answer.
-    var maxTokens =  60// Length of output generated. 1 token is on average ~4 characters or 0.75 words for English text
-    var topP = 1.0 // 1.0 is default. An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
-    var frequencyPenalty = 0.0 // Number between -2.0 and 2.0. Positive values penalize new tokens based on their existing frequency in the text so far, decreasing the model's likelihood to repeat the same line verbatim.
-    var presencePenalty = 0.0 // Number between -2.0 and 2.0. Positive values penalize new tokens based on whether they appear in the text so far, increasing the model's likelihood to talk about new topics.
-    val completionRequest = CompletionRequest.builder()
-        .temperature(temperature)
-        .topP(topP)
-        .frequencyPenalty(frequencyPenalty)
-        .presencePenalty(presencePenalty)
-        .maxTokens(maxTokens)
-        .prompt(prompt)
-        .echo(true)
-        .build();
-    try {
-        println(history)
-        val completion = service.createCompletion("text-davinci-003", completionRequest).getChoices().first().text
-        response = completion.trim()
-    } catch (e: Exception) {
-        println("Problem with connection to OpenAI")
-    }
-    return response.drop(lengthofprompt)
-} */
 fun getNLGResponseFromGPT(input: String): String {
 
     val service = OpenAiService(serviceKey)
@@ -92,9 +50,9 @@ fun getNLGResponseFromGPT(input: String): String {
     } catch (e: Exception) {
         println("Problem with connection to OpenAI" + e.message)
     }
-    println(response)
-    println("-------------------------------------------------")
-    println(response.drop(lengthofprompt))
+    //println(response)
+    //println("-------------------------------------------------")
+    //println(response.drop(lengthofprompt))
     return response.drop(lengthofprompt)
 }
 
